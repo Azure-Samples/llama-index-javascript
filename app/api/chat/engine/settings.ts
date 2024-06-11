286953b26e42e2da5570f5c152dffe20e3f7b42e
Wassim Chegham <github@wassim.dev>
2024-06-11 15:28:00 +0200

    chore: update LlamaIndex settings to use Azure OpenAI MI config

--- app/api/chat/engine/settings.ts
+++ app/api/chat/engine/settings.ts
@@ -1,48 +1,49 @@
-import {
-  Anthropic,
-  GEMINI_EMBEDDING_MODEL,
-  GEMINI_MODEL,
-  Gemini,
-  GeminiEmbedding,
-  OpenAI,
-  OpenAIEmbedding,
-  Settings,
-} from "llamaindex";
-import {AzureOpenAI} from "openai";
+import { OpenAI, OpenAIEmbedding, Settings } from "llamaindex";
 
-import { DefaultAzureCredential, getBearerTokenProvider } from "@azure/identity";
-import { HuggingFaceEmbedding } from "llamaindex/embeddings/HuggingFaceEmbedding";
+import {
+  DefaultAzureCredential,
+  getBearerTokenProvider,
+} from "@azure/identity";
 import { OllamaEmbedding } from "llamaindex/embeddings/OllamaEmbedding";
-import { ALL_AVAILABLE_ANTHROPIC_MODELS } from "llamaindex/llm/anthropic";
 import { Ollama } from "llamaindex/llm/ollama";
 
 const CHUNK_SIZE = 512;
 const CHUNK_OVERLAP = 20;
+const AZURE_COGNITIVE_SERVICES_SCOPE =
+  "https://cognitiveservices.azure.com/.default";
 
 export const initSettings = async () => {
-  // HINT: you can delete the initialization code for unused model providers
   console.log(`Using '${process.env.MODEL_PROVIDER}' model provider`);
 
-  if (!process.env.MODEL || !process.env.EMBEDDING_MODEL) {
-    throw new Error("'MODEL' and 'EMBEDDING_MODEL' env variables must be set.");
+  // if provider is OpenAI, MODEL must be set
+  if (process.env.MODEL_PROVIDER === 'openai' && process.env.OPENAI_API_TYPE !== 'azure' && !process.env.MODEL) {
+    throw new Error("'MODEL' env variable must be set.");
+  }
+
+  // if provider is Azure OpenAI, AZURE_DEPLOYMENT_NAME must be set
+  if (process.env.MODEL_PROVIDER === 'openai' && process.env.OPENAI_API_TYPE === 'azure' && !process.env.AZURE_DEPLOYMENT_NAME) {
+    throw new Error("'AZURE_DEPLOYMENT_NAME' env variables must be set.");
+  }
+
+  if (!process.env.EMBEDDING_MODEL) {
+    throw new Error("'EMBEDDING_MODEL' env variable must be set.");
   }
 
   switch (process.env.MODEL_PROVIDER) {
     case "ollama":
       initOllama();
       break;
-    case "anthropic":
-      initAnthropic();
-      break;
-    case "gemini":
-      initGemini();
-      break;
-    case "azureopenai":
-      initAzureOpenAI();
+    case "openai":
+      if (process.env.OPENAI_API_TYPE === "azure") {
+        await initAzureOpenAI();
+      } else {
+        initOpenAI();
+      }
       break;
     default:
-      initOpenAI();
-      break;
+      throw new Error(
+        `Model provider '${process.env.MODEL_PROVIDER}' not supported.`,
+      );
   }
   Settings.chunkSize = CHUNK_SIZE;
   Settings.chunkOverlap = CHUNK_OVERLAP;
@@ -61,14 +62,27 @@ function initOpenAI() {
   });
 }
 
-function initAzureOpenAI() {
+async function initAzureOpenAI() {
   const credential = new DefaultAzureCredential();
-  const azureADTokenProvider = getBearerTokenProvider(credential, "https://cognitiveservices.azure.com/.default");
-  Settings.llm = new AzureOpenAI({
+  const azureADTokenProvider = getBearerTokenProvider(
+    credential,
+    AZURE_COGNITIVE_SERVICES_SCOPE,
+  );
+
+  const azure = {
     azureADTokenProvider,
-    deployment: process.env.MODEL ?? "gpt-35-turbo",
+    deployment: process.env.AZURE_OPENAI_DEPLOYMENT ?? "gpt-35-turbo",
+  };
+
+  // configure LLM model
+  Settings.llm = new OpenAI({
+    azure,
   }) as any;
+
+  // configure embedding model
+  azure.deployment = process.env.EMBEDDING_MODEL as string;
   Settings.embedModel = new OpenAIEmbedding({
+    azure,
     model: process.env.EMBEDDING_MODEL,
     dimensions: process.env.EMBEDDING_DIM
       ? parseInt(process.env.EMBEDDING_DIM)
@@ -90,24 +104,3 @@ function initOllama() {
   });
 }
 
-function initAnthropic() {
-  const embedModelMap: Record<string, string> = {
-    "all-MiniLM-L6-v2": "Xenova/all-MiniLM-L6-v2",
-    "all-mpnet-base-v2": "Xenova/all-mpnet-base-v2",
-  };
-  Settings.llm = new Anthropic({
-    model: process.env.MODEL as keyof typeof ALL_AVAILABLE_ANTHROPIC_MODELS,
-  });
-  Settings.embedModel = new HuggingFaceEmbedding({
-    modelType: embedModelMap[process.env.EMBEDDING_MODEL!],
-  });
-}
-
-function initGemini() {
-  Settings.llm = new Gemini({
-    model: process.env.MODEL as GEMINI_MODEL,
-  });
-  Settings.embedModel = new GeminiEmbedding({
-    model: process.env.EMBEDDING_MODEL as GEMINI_EMBEDDING_MODEL,
-  });
-}
