aaad36dddc63f65ae282dd4bbd3869322e255aa0
Wassim Chegham <github@wassim.dev>
2024-06-21 14:45:50 +0200

    fix: regex and remove local llamaindex patch from dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,6 @@ FROM node:20-alpine as build
 
 WORKDIR /app
 
-# Install dependencies
-#TODO: send a PR to LlamaIndex.ts upstrea and remove this patch
-COPY llamaindex-0.3.16-aoai-mi.tgz ./
-
 COPY package.json package-lock.* ./
 RUN npm install
 
--- app/components/ui/chat/markdown.tsx
+++ app/components/ui/chat/markdown.tsx
@@ -17,12 +17,12 @@ const MemoizedReactMarkdown: FC<Options> = memo(
 const preprocessLaTeX = (content: string) => {
   // Replace block-level LaTeX delimiters \[ \] with $$ $$
   const blockProcessedContent = content.replace(
-    /\\\[(.*?)\\\]/gs,
+    /\\\[(.*?)\\\]/g,
     (_, equation) => `$$${equation}$$`,
   );
   // Replace inline LaTeX delimiters \( \) with $ $
   const inlineProcessedContent = blockProcessedContent.replace(
-    /\\\((.*?)\\\)/gs,
+    /\\\((.*?)\\\)/g,
     (_, equation) => `$${equation}$`,
   );
   return inlineProcessedContent;
